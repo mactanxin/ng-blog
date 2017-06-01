2fe9a365e5a56eb75bbdb42fc9ffd69c9ef8a586
Xin Tan <mactanxin@gmail.com>
2017-05-31 22:46:03 -0700

    UPDATED: use angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,34 @@ import { Component } from '@angular/core';
 import { Post } from './post';
 
 import { AppRoutingModule } from './app-routing.module';
-const POSTS: Post[] = [{
-userId: 1,
-id: 1,
-title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-},
-{
-userId: 1,
-id: 2,
-title: "qui est esse",
-body: "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
-},
-{
-userId: 1,
-id: 3,
-title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-body: "et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut"
-},
-{
-userId: 10,
-id: 100,
-title: "at nam consequatur ea labore ea harum",
-body: "cupiditate quo est a modi nesciunt soluta ipsa voluptas error itaque dicta in autem qui minus magnam et distinctio eum accusamus ratione error aut"
-}
+import {MdButtonModule, MdCheckboxModule, MdListModule} from '@angular/material';
+
+
+const POSTS: Post[] = [
+    {
+        userId: 1,
+        id: 1,
+        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+        userId: 1,
+        id: 2,
+        title: "qui est esse",
+        body: "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+        userId: 1,
+        id: 3,
+        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        body: "et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut"
+    },
+    {
+        userId: 10,
+        id: 100,
+        title: "at nam consequatur ea labore ea harum",
+        body: "cupiditate quo est a modi nesciunt soluta ipsa voluptas error itaque dicta in autem qui minus magnam et distinctio eum accusamus ratione error aut"
+    }
 ]
 
 @Component({
