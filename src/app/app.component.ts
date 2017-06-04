30383c6049fc69de4e9f57b80f57eb0157945ff3
Xin Tan <mactanxin@gmail.com>
2017-06-04 10:28:35 -0700

    removed all static mock posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from './post';
 
 import { AppRoutingModule } from './app-routing.module';
 import {MdButtonModule, MdCheckboxModule, MdListModule, MdCardModule, MdInputModule} from '@angular/material';
 
+import { PostsService } from './posts.service';
 
-const POSTS: Post[] = [
-    {
-        userId: 1,
-        id: 1,
-        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-        userId: 1,
-        id: 2,
-        title: "qui est esse",
-        body: "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
-    },
-    {
-        userId: 1,
-        id: 3,
-        title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        body: "et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut"
-    },
-    {
-        userId: 10,
-        id: 100,
-        title: "at nam consequatur ea labore ea harum",
-        body: "cupiditate quo est a modi nesciunt soluta ipsa voluptas error itaque dicta in autem qui minus magnam et distinctio eum accusamus ratione error aut"
-    }
-]
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[PostsService]
 })
 
-export class AppComponent {
-  posts = POSTS;
+export class AppComponent implements OnInit{
+  posts = [];
 
   selectedPost: Post;
 
+  constructor(protected postService: PostsService){};
   onSelect(post: Post): void {
       this.selectedPost = post;
+  }
+
+
+  getPosts() {
+    this.postService.getPosts().then(posts => this.posts = posts);;
+  }
+  ngOnInit() :void {
+      this.getPosts()
   }
 }
