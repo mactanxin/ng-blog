d7699f8a154bb5196059acee0e172fdbdbaadfb3
Xin Tan <mactanxin@gmail.com>
2017-06-04 10:29:21 -0700

    load all posts from static mock-post

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -3,22 +3,30 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { Post } from './post';
+import { POSTS } from './mock-posts';
 
 @Injectable()
 export class PostsService {
     postsUrl: string = 'https://jsonplaceholder.typicode.com/posts';
 
-    constructor(public http: Http) { }
+    constructor(public http: Http){}
 
-    getPosts() {
-      return this.http.get(this.postsUrl).map( res => res.json() );
+    getPosts(): Promise<Post[]>{
+      // return this.http.get(this.postsUrl).map( res => res.json() );
+      return Promise.resolve(POSTS);
     }
 
-    //récupération d'un post
     getPost(id) {
 
       const post = `${this.postsUrl}/${id}`;
 
       return this.http.get(post).map(res => res.json());
+    }
+
+    getPostsSlowly(): Promise<Post[]> {
+      return new Promise(resolve => {
+        setTimeout(() => resolve(this.getPosts()), 2000);
+      });
     }
 }
